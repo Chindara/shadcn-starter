5c7e7bf9b325988db701b46ae5d860013c5af590
Chinthaka Bandara <chinthakapb@gmail.com>
2026-01-22 15:48:47 +0530

    style: pageTitle padding updated

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -5,7 +5,7 @@ interface PageTitleProps {
 
 const PageTitle = ({ title, subTitle }: PageTitleProps) => {
   return (
-    <div className="flex justify-between items-center mb-4">
+    <div className="flex justify-between items-center mb-1">
       <div className="flex items-left flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
         <h6 className="text-sm text-muted-foreground">{subTitle}</h6>
